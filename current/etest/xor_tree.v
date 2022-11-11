b08d9afef5d6114c29df1028bde61dcd4228739d
eshellko <eshell@eshell.com>
2022-11-11 15:42:03 +0300

    * typo

--- current/etest/xor_tree.v
+++ current/etest/xor_tree.v
@@ -4,7 +4,7 @@ module xor_tree
    output q
 );
 // Note: there are 15 inputs to XOR tree
-//       thus 1/8, 1/4, 1/2, and final - 4 level with delay 2 each on ba;anced tree = 8 gates delay
+//       thus 1/8, 1/4, 1/2, and final - 4 level with delay 2 each on balanced tree = 8 gates delay
 assign q = a^ b^c^d^e^f^g^h^i^j^k^l^m^n^o;
 
 endmodule
